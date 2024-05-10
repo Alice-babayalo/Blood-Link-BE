b9b7e5663fed73bd900dc0895e2f8ae50ab858a3
Alice-babayalo <umuhozawasealice@gmail.com>
2024-05-10 13:28:31 +0200

    Update: swagger documentation for admin signup, sign in, forgot password and reset password

--- services/app.js
+++ services/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDb from './db.js';
 import ErrorHandlerMiddleware from '../middleware/ErrorHandler.js'
+import cors from "cors"
 import {routes} from '../routes/index.js'
 import documentation from '../docs/swagger.json' assert {"type":"json"}
 import swaggerUi from 'swagger-ui-express'
@@ -10,7 +11,7 @@ const app = express();
 
 
 
-
+app.use(cors())
 
 
 app.use(express.json())
