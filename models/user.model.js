8ef8815f288aaa1db15ef6d660c50e5edc4980ed
Alice-babayalo <umuhozawasealice@gmail.com>
2024-05-15 15:06:32 +0200

    modifying the role users

--- models/user.model.js
+++ models/user.model.js
@@ -16,8 +16,8 @@ const AdminSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["donar", "hospital"],
-    default: "donar",
+    enum: ["donor", "hospital"],
+    default: "donor",
   },
 
   otp: {
