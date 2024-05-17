8d43e23cadd361fb1a179fd0fe622f5903268382
NIYONIZERA <benigneniyonizera58@gmail.com>
2024-05-17 15:41:49 +0200

    Authorization

--- routes/donor.route.js
+++ routes/donor.route.js
@@ -9,10 +9,12 @@ import {
   
 } from '../controllers/donor.controller.js';
 import {createDonorValidationRules } from '../utils/validation.js';
+import { authMiddleware } from '../middleware/authorization.js';
 
 const router = express.Router();
 
 // Routes
+router.use(authMiddleware);
 router.post('/create',createDonorValidationRules, createDonor);
 router.get('/getDonor', listDonors);
 router.get('/gerDonor/:id', getDonorById);
